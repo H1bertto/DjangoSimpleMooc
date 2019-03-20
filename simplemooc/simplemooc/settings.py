c5353c85eae0448ec2460629dc08ffbf5d695e22
humberto.v.souza <humberto.v.souza@accenture.com>
2019-03-20 18:28:06 -0300

    Migrate e SQL Ajudstados

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'coreutils',
+    'coreutils.apps.CoreutilsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
