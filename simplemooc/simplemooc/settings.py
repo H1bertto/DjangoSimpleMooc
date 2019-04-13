c1c6dcf61490f04441c4e77805cd66f812fb8c70
Humberto <humbertovieiras@hotmail.com>
2019-04-13 19:02:53 -0300

    Teste 2

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -131,3 +131,15 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
+
+# E-Mails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'The Gambiarra <thegambiarra2@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'thegambiarra2@gmail.com'
+EMAIL_HOST_PASSWORD = 'PythonTGG2'
+EMAIL_PORT = 587
+
+CONTACT_EMAIL = 'thegambiarra2@gmail.com'
